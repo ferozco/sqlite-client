6a604e5ad568f410d17daa1b54f435c37c78ac43
Felipe <felipe@theoremlp.com>
2024-07-23 20:33:34 -0400

    Inital commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require('webpack')
+
+module.exports = {
+    mode: "development",
+    entry: "./src/main/ts/index.tsx",
+    output: {
+        path: path.join(__dirname, "build/assets"),
+        filename: "bundle.[contenthash].js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"],
+        modules: ["node_modules"],
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|png|svg|json)$/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.js(\.map)?$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            path: path.join(__dirname, "build/assets"),
+            filename: "index.html",
+            template: "./src/main/html/index.html",
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: "./build/assets",
+        },
+        historyApiFallback: true,
+        compress: true,
+        port: 8081,
+        server: "https",
+    },
+};
